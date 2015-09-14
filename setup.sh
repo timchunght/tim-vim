56b1ce91db26364a68209b7ab59de5034a8823df
Timothy Chung <timchunght@gmail.com>
2015-09-13 23:14:19 -0400

    Configured setup.sh to enable reinstallation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 cp .vimrc ~/
+
+# Remove directories
+rm -rf ~/.vim/bundle/*
 # Install vundle, a vim plugin management system
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # Install all vim plugins
 vim +PluginInstall +qall
